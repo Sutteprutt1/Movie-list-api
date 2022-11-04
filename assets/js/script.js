00bb02e955b5e35a42e49ce2b9b1612ea7898bf7
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2022-11-04 10:56:20 +0100

    tried something

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,18 @@
 fetch('https://api.tvmaze.com/shows ').then((data) => {
     return data.json();
 }).then((completedata) => {
-    console.log(completedata);
+    console.log(completedata.data);
 
     let data1 = "";
-    completedata.map((values) => {
+    completedata.map.data((values) => {
         data1= 
         ` <div class="card">
             <img src=${values.image} class="images" alt="img">
             <h2 class="titel">${values.name}</h2>
             <p class="time">${values.runtime}</p>
             <h2 class="genre">${values.genres}</h2>
-            <p class="rating"${values.rating}>Jobtitle<p>
-        </div> `
+            <p class="rating"${values.rating}><p>
+            </div> `
     });
     document.getElementById("cards").innerHTML = data1;
 }).catch((err) => {
