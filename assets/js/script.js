c5c4b6e1f63a1750808c66e39eb2f1604663af6f
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2022-11-06 15:07:46 +0100

    lots of styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,26 @@ fetch('https://api.tvmaze.com/shows').then((data) => {
 
     let data1 = "";
     completedata.map((values) => {
-        data1= 
-        ` <div class="card">
-            <img src=${values.image.original} class="images" alt="img">
-            <h2 class="titel">${values.name}</h2>
-            <h2 class="time">${values.runtime} min</h2>
-            <p class="genres">${values.genres}<p>
-            <p class="rating">${values.rating.average}</p>
-        </div> `
+        data1 +=
+        `<div class="card_item">
+                <div class="card_inner">
+                    <img class="image" src=${values.image.original}  alt="img">
+                    <h2 class="titel">${values.name}</h2>
+                    <h2 class="time">${values.runtime} min</h2>
+                    <p class="genres">${values.genres}<p>
+                    <p class="rating">${values.rating.average}</p>
+                </div>
+        </div>`
     });
-    document.getElementById("cards").innerHTML = data1;
+    document.getElementById("cards_wrap").innerHTML = data1;
 }).catch((err) => {
     console.log(err);
 })
+
+
+// /* 
+// img = image
+// role name = titel
+// real name = time
+// film = genres + rating
+// */
